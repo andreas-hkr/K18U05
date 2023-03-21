ca1f1de7c6610ff69c131573e220ed9ec95ae543
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-21 15:47:30 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // - Använd klasserna Klockslag och Observation som studerats tidigare
+        // - Läs in observationer från fil och spara i en lista
+        //   (stationsid tim min temperatur vindhastighet vindriktning)
+        // - Sortera i tidsordning
+        // - Sortera efter stationsid
+        // - Skriv ut stationsid, tid, och temperatur efter varje sortering
+
+
+    }
+}
